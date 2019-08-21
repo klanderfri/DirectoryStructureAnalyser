fad9d68dff707423c5b566927ee2b9afec4fb998
klanderfri <registrering+github@bjornlarsson.se>
2019-08-21 20:24:28 +0200

    Initial code.

---
+++ DirectoryStructureAnalyser/AnalyserForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DirectoryStructureAnalyser
+{
+    partial class AnalyserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxRootDirectoryPath = new System.Windows.Forms.TextBox();
+            this.btnSelectRootDirectory = new System.Windows.Forms.Button();
+            this.btnAnalyseStructure = new System.Windows.Forms.Button();
+            this.twDirectoryTree = new System.Windows.Forms.TreeView();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.SuspendLayout();
+            // 
+            // tbxRootDirectoryPath
+            // 
+            this.tbxRootDirectoryPath.Location = new System.Drawing.Point(12, 12);
+            this.tbxRootDirectoryPath.Name = "tbxRootDirectoryPath";
+            this.tbxRootDirectoryPath.Size = new System.Drawing.Size(465, 20);
+            this.tbxRootDirectoryPath.TabIndex = 0;
+            // 
+            // btnSelectRootDirectory
+            // 
+            this.btnSelectRootDirectory.Location = new System.Drawing.Point(483, 10);
+            this.btnSelectRootDirectory.Name = "btnSelectRootDirectory";
+            this.btnSelectRootDirectory.Size = new System.Drawing.Size(129, 23);
+            this.btnSelectRootDirectory.TabIndex = 1;
+            this.btnSelectRootDirectory.Text = "Select Root Directory";
+            this.btnSelectRootDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectRootDirectory.Click += new System.EventHandler(this.btnSelectRootDirectory_Click);
+            // 
+            // btnAnalyseStructure
+            // 
+            this.btnAnalyseStructure.Location = new System.Drawing.Point(618, 10);
+            this.btnAnalyseStructure.Name = "btnAnalyseStructure";
+            this.btnAnalyseStructure.Size = new System.Drawing.Size(126, 23);
+            this.btnAnalyseStructure.TabIndex = 2;
+            this.btnAnalyseStructure.Text = "Analyse Structure";
+            this.btnAnalyseStructure.UseVisualStyleBackColor = true;
+            this.btnAnalyseStructure.Click += new System.EventHandler(this.btnAnalyseStructure_Click);
+            // 
+            // twDirectoryTree
+            // 
+            this.twDirectoryTree.Location = new System.Drawing.Point(12, 54);
+            this.twDirectoryTree.Name = "twDirectoryTree";
+            this.twDirectoryTree.Size = new System.Drawing.Size(732, 807);
+            this.twDirectoryTree.TabIndex = 3;
+            this.twDirectoryTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.twDirectoryTree_NodeMouseDoubleClick);
+            // 
+            // AnalyserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 873);
+            this.Controls.Add(this.twDirectoryTree);
+            this.Controls.Add(this.btnAnalyseStructure);
+            this.Controls.Add(this.btnSelectRootDirectory);
+            this.Controls.Add(this.tbxRootDirectoryPath);
+            this.Name = "AnalyserForm";
+            this.Text = "Directory Structure Analyser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxRootDirectoryPath;
+        private System.Windows.Forms.Button btnSelectRootDirectory;
+        private System.Windows.Forms.Button btnAnalyseStructure;
+        private System.Windows.Forms.TreeView twDirectoryTree;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+    }
+}
+
---
+++ DirectoryStructureAnalyser/AnalyserForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryStructureAnalyser
+{
+    public partial class AnalyserForm : Form
+    {
+        public AnalyserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectRootDirectory_Click(object sender, EventArgs e)
+        {
+            using (var directorySelecter = new FolderBrowserDialog())
+            {
+                if (directorySelecter.ShowDialog() == DialogResult.OK)
+                {
+                    tbxRootDirectoryPath.Text = directorySelecter.SelectedPath;
+                }
+            }
+        }
+
+        private void btnAnalyseStructure_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(tbxRootDirectoryPath.Text))
+            {
+                twDirectoryTree.Nodes.Clear();
+                var root = new Folder(tbxRootDirectoryPath.Text);
+                addDirectoryToTree(root, twDirectoryTree.Nodes);
+                twDirectoryTree.Nodes[0].Expand();
+            }
+        }
+
+        private void addDirectoryToTree(Folder directory, TreeNodeCollection parentNode)
+        {
+            var descriptionFormat = "{0} - {1} MB";
+            var description = String.Format(descriptionFormat, directory.Info.Name, directory.SizeInMB);
+            var directoryNode = addNode(parentNode, description, directory.SizeInMB, directory);
+
+            var underline = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline, GraphicsUnit.Point);
+            directoryNode.NodeFont = underline;
+
+            foreach (var child in directory.SubFolders)
+            {
+                addDirectoryToTree(child, directoryNode.Nodes);
+            }
+
+            foreach (var file in directory.Info.GetFiles())
+            {
+                var sizeInMB = Converter.MbFromByte(file.Length);
+                var fileDescription = String.Format(descriptionFormat, file.Name, sizeInMB);
+                addNode(directoryNode.Nodes, fileDescription, sizeInMB, file);
+            }
+        }
+
+        private TreeNode addNode(TreeNodeCollection parent, string description, double sizeInMB, object tag)
+        {
+            var node = parent.Add(description);
+            node.Tag = tag;
+
+            if (sizeInMB > 100)
+            {
+                node.ForeColor = Color.Red;
+            }
+
+            return node;
+        }
+
+        private void twDirectoryTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            var directory = e.Node.Tag as Folder;
+            var file = e.Node.Tag as FileInfo;
+            if (directory != null)
+            {
+                Process.Start(directory.Info.FullName);
+            }
+            else if (file != null)
+            {
+                Process.Start(file.Directory.FullName);
+            }
+        }
+    }
+
+    public class Converter
+    {
+        public static double MbFromByte(long sizeInBytes)
+        {
+            return Math.Round(sizeInBytes / Math.Pow(1024, 2), 1);
+        }
+    }
+
+    public class Folder
+    {
+        public DirectoryInfo Info { get; private set; }
+        public long SizeInBytes { get; private set; }
+        public double SizeInMB
+        {
+            get { return Converter.MbFromByte(SizeInBytes); }
+        }
+
+        public List<Folder> SubFolders { get; private set; }
+
+        public Folder(string folderpath)
+        {
+            Info = new DirectoryInfo(folderpath);
+            fillSubfolders();
+            calculateSize();
+        }
+
+        private void fillSubfolders()
+        {
+            SubFolders = new List<Folder>();
+
+            foreach (var child in Info.GetDirectories())
+            {
+                SubFolders.Add(new Folder(child.FullName));
+            }
+        }
+
+        private void calculateSize()
+        {
+            SizeInBytes = 0;
+
+            foreach (var child in SubFolders)
+            {
+                SizeInBytes += child.SizeInBytes;
+            }
+
+            var files = Info.GetFiles();
+            foreach (var file in files)
+            {
+                SizeInBytes += file.Length;
+            }
+        }
+    }
+}
