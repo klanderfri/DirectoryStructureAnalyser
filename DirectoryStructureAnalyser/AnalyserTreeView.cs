6289439dfaac409c47c30568c1e824a6a6244edc
klanderfri <registrering+github@bjornlarsson.se>
2020-01-29 13:53:27 +0100

    Made it possible for the user to select colour.

--- DirectoryStructureAnalyser/AnalyserTreeView.cs
+++ DirectoryStructureAnalyser/AnalyserTreeView.cs
@@ -15,7 +15,7 @@ namespace DirectoryStructureAnalyser
         /// <param name="settings">An object representing the settings for adding the folder structure.</param>
         public void AddFileFolderStructure(AnalyseSettings settings)
         {
-            addDirectoryToTree(settings.RootFolder, Nodes, settings.BigFolderMbyteSize);
+            addDirectoryToTree(settings.RootFolder, Nodes, settings);
         }
 
         /// <summary>
@@ -23,26 +23,26 @@ namespace DirectoryStructureAnalyser
         /// </summary>
         /// <param name="directory">The folder to add to the tree view.</param>
         /// <param name="parentNode">The parent node collection.</param>
-        /// <param name="bigFoldersizeInMB">The size in MB when a folder is considered big.</param>
-        private void addDirectoryToTree(Folder directory, TreeNodeCollection parentNode, int bigFoldersizeInMB)
+        /// <param name="settings">The settings for the folder structure analyse.</param>
+        private void addDirectoryToTree(Folder directory, TreeNodeCollection parentNode, AnalyseSettings settings)
         {
             var descriptionFormat = "{0} - {1} MB";
             var description = String.Format(descriptionFormat, directory.Info.Name, directory.SizeInMB);
-            var directoryNode = addNode(parentNode, description, directory.SizeInMB, bigFoldersizeInMB, directory);
+            var directoryNode = addNode(parentNode, description, directory.SizeInMB, settings, directory);
 
             var underline = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline, GraphicsUnit.Point);
             directoryNode.NodeFont = underline;
 
             foreach (var child in directory.SubFolders)
             {
-                addDirectoryToTree(child, directoryNode.Nodes, bigFoldersizeInMB);
+                addDirectoryToTree(child, directoryNode.Nodes, settings);
             }
 
             foreach (var file in directory.Info.GetFiles())
             {
                 var sizeInMB = ByteSizeConverter.MbFromByte(file.Length);
                 var fileDescription = String.Format(descriptionFormat, file.Name, sizeInMB);
-                addNode(directoryNode.Nodes, fileDescription, sizeInMB, bigFoldersizeInMB, file);
+                addNode(directoryNode.Nodes, fileDescription, sizeInMB, settings, file);
             }
         }
 
@@ -52,17 +52,17 @@ namespace DirectoryStructureAnalyser
         /// <param name="parent">The parent node collection.</param>
         /// <param name="description">The description of the node.</param>
         /// <param name="sizeInMB">The size in MB of the node and its subnodes.</param>
-        /// <param name="bigFoldersizeInMB">The size in MB when a folder is considered big.</param>
+        /// <param name="settings">The settings for the folder structure analyse.</param>
         /// <param name="tag">An object to hang on to the node.</param>
         /// <returns>The added tree node.</returns>
-        private static TreeNode addNode(TreeNodeCollection parent, string description, double sizeInMB, int bigFoldersizeInMB, object tag)
+        private static TreeNode addNode(TreeNodeCollection parent, string description, double sizeInMB, AnalyseSettings settings, object tag)
         {
             var node = parent.Add(description);
             node.Tag = tag;
 
-            if (sizeInMB >= bigFoldersizeInMB)
+            if (sizeInMB >= settings.BigFolderMbyteSize)
             {
-                node.ForeColor = Color.Red;
+                node.ForeColor = settings.BigFolderColour;
             }
 
             return node;
